9832e47e36524010a186317498f16d03f80c896b
Janie <evgeniyaberseneva89@gmail.com>
2019-06-11 15:10:34 +0300

    router structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import DefaultPage from "./pages/DefaultPage";
 import AboutPage from "./pages/AboutPage";
@@ -11,13 +12,16 @@ import CartPage from "./pages/CartPage";
 function App() {
   return (
     <>
-      <HomePage />
-      <DefaultPage />
-      <AboutPage />
-      <ProductsPage />
-      <SingleProductPage />
-      <ContactPage />
-      <CartPage />
+      {/* navbar here */}
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/products" exact component={ProductsPage} />
+        <Route path="/products/:id" component={SingleProductPage} />
+        <Route path="/contact" component={ContactPage} />
+        <Route path="/cart" component={CartPage} />
+        <Route component={DefaultPage} />
+      </Switch>
     </>
   );
 }
