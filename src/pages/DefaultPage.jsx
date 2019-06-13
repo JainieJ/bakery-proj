92f8ea2ccb71da66ca314af74ceafb90a74da257
Janie <evgeniyaberseneva89@gmail.com>
2019-06-13 15:45:48 +0300

    Default Page complete

--- src/pages/DefaultPage.jsx
+++ src/pages/DefaultPage.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import Hero from "../components/Hero";
+import defaultBcg from "../img/defaultBcg.jpg";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const DefaultPage = () => {
-  return <h1>hello from DefaultPage</h1>;
+  return (
+    <DefaultWrapper>
+      <Hero img={defaultBcg} fullHeight="true">
+        <div className="default-title">
+          <h3 className="error">404</h3>
+          <h3>page not found</h3>
+        </div>
+        <Link to="/" className="main-link">
+          back home
+        </Link>
+      </Hero>
+    </DefaultWrapper>
+  );
 };
 
 export default DefaultPage;
+
+const DefaultWrapper = styled.div`
+  .default-title {
+    margin-bottom: 3rem;
+    margin-top: -8rem;
+  }
+  h3 {
+    font-size: 2.5rem;
+    text-transform: capitalize;
+  }
+  .error {
+    color: var(--mainPink);
+  }
+`;
